3f6e868aff5b576d96588fef13c04b6e08225f3a
ob1991 <369453042@qq.com>
2017-06-01 21:38:56 +0800

    登录已完成

--- app/src/main/java/com/amap/location/demo/main.java
+++ app/src/main/java/com/amap/location/demo/main.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.util.Log;
 import android.view.View;
 import android.view.Window;
@@ -12,32 +14,43 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.amap.location.demo.DB.NewService;
-
-import org.json.JSONException;
-import org.json.JSONObject;
+import com.amap.location.demo.DB.NetUtils;
 
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Admin on 2017/05/24.
  */
 
 public class main extends Activity {
-
+    private final String url="http://202.118.16.50:8101/locationlogin.ashx";
     private Button bt1;
     private Button bt2;
-    private Bundle savedInstanceState;
     private EditText textname;
     private EditText textpassword;
-    private ProgressDialog loginProgress;
+    private Handler handler=new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            // TODO Auto-generated method stub
+            if (msg.what==1) {
+                if(msg.obj!=null&& msg.obj.toString().equals("1")){
+                    Toast.makeText(main.this, R.string.ok, Toast.LENGTH_SHORT).show();
+                    Intent intent =new Intent(main.this,LocationModeSourceActivity.class);
+                    startActivity(intent);
+                }else{
+                    Toast.makeText(main.this, R.string.error, Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
-        this.savedInstanceState = savedInstanceState;
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.login);
@@ -55,37 +68,44 @@ public class main extends Activity {
         });
 
     }
+    class sendValueToServer implements Runnable
+    {
+        Map<String, String> map;
+        public sendValueToServer(Map<String, String> map) {
+            this.map=map;
+        }
 
-    private class mybuttonlistener implements View.OnClickListener {
-        boolean result=false;
+        @Override
+        public void run() {
+            String result = NetUtils.getRequest(url, map);
+            Message message = Message.obtain(handler, 1, result);
+            handler.sendMessage(message);
+        }
+    }
+
+    private void btnget()
+    {
         String name;
         String password;
+        try {
+            name = textname.getText().toString();
+            name = new String(name.getBytes("ISO8859-1"), "UTF-8");
+            password = textpassword.getText().toString();
+            password = new String(password.getBytes("ISO8859-1"), "UTF-8");
+            Map<String, String> map=new HashMap<String, String>();
+            map.put("name",name);
+            map.put("password", password);
+            new Thread(new sendValueToServer(map)).start();
+        } catch (UnsupportedEncodingException e1) {
+            // TODO Auto-generated catch block
+            e1.printStackTrace();
+        }
+    }
+    private class mybuttonlistener implements View.OnClickListener {
+
+        @Override
         public void onClick(View v) {
-            try {
-                name = textname.getText().toString();
-                name = new String(name.getBytes("ISO8859-1"), "UTF-8");
-                password = textpassword.getText().toString();
-                password = new String(password.getBytes("ISO8859-1"), "UTF-8");
-            } catch (UnsupportedEncodingException e1) {
-                // TODO Auto-generated catch block
-                e1.printStackTrace();
-            }
-            new Thread(new Runnable() {
-                @Override
-                public void run() {
-            try {
-                result = NewService.save(name,password,"http://202.118.16.50:8101/locationlogin.ashx");
-            } catch (Exception e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
-            if(result){
-                Toast.makeText(main.this, R.string.ok, Toast.LENGTH_SHORT).show();
-            }else{
-                Toast.makeText(main.this, R.string.error, Toast.LENGTH_SHORT).show();
-            }
-                }
-            }).start();
+            btnget();
         }
     }
 }
